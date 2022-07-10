abbaa474de90a655c8be1903bc139dcd967c91db
choegyuhyeon <rbgus2002@naver,.com>
2022-07-10 21:34:04 +0900

    .

---
+++ 2weeks/BaekJoon_1931.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+
+
+
+    }
+}
+
+
