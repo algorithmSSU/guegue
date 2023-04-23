50fb4351dc9947f8c46f6f6a01bab38db676e68f
rbgus2002 <rbgus2002@naver.com>
2023-04-23 12:04:26 +0900

    BOJ1167 : 트리의 지름

--- Baekjoon/BOJ1167.java
+++ Baekjoon/BOJ1167.java
@@ -19,7 +19,7 @@ public class BOJ1167 {
     private static boolean[] isVisited;
     private static List<Edge>[] tree;
     private static List<Integer> candidateVertex;
-    private static int n, answer;
+    private static int n, end, answer;
 
     public static void main(String[] args) throws IOException {
         init();
@@ -61,17 +61,21 @@ public class BOJ1167 {
 
         // init answer
         answer = Integer.MIN_VALUE;
+
+        //
+        end = -1;
     }
 
     private static void setTreeDiameter(){
-        for(int i = 1; i <= n; i++){
-            if(tree[i].size() == 1)
-                dfs(i, 0);
-        }
+        dfs(1, 0);
+        dfs(end, 0);
     }
 
     private static void dfs(int idx, int weightSum){
-        answer = Math.max(answer, weightSum);
+        if(weightSum > answer){
+            answer = weightSum;
+            end = idx;
+        }
         isVisited[idx] = true;
 
         for(Edge adjacency : tree[idx]){
@@ -83,3 +87,11 @@ public class BOJ1167 {
         isVisited[idx] = false;
     }
 }
+
+
+/**
+ * 트리의 지름을 구하는 공식
+ * 1. 임의의 노드 X를 선택하고 X로부터 가장 먼 거리의 노드 V를 찾는다.
+ * 2. V로부터 가장 먼 거리의 노드 T를 찾는다.
+ * 3. V와 T 사이의 거리가 곧 트리의 지름이다.
+ */
